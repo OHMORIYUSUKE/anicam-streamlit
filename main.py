5de2e9a4609f0044564776196b829174de981c2c
OHMORIYUSUKE <72294945+OHMORIYUSUKE@users.noreply.github.com>
2020-12-29 19:49:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,37 @@ st.title('アイドルに変身アプリ')
 - 開始ボタンをクリックすると、カメラが起動します。終了したい場合は終了ボタン**ダブルクリック**してください。
 - システムの仕様上ダブルクリックが認識されない場合があります。その際はもう一度終了ボタンをクリックしてください。
 """
+
+
+def check_camera_connection():
+    """
+    Check the connection between any camera and the PC.
+
+    """
+
+    print('[', datetime.datetime.now(), ']', 'searching any camera...')
+    true_camera_is = []  # 空の配列を用意
+
+    # カメラ番号を0～9まで変えて、COM_PORTに認識されているカメラを探す
+    for camera_number in range(0, 10):
+        cap = cv2.VideoCapture(camera_number)
+        ret, frame = cap.read()
+
+        if ret is True:
+            true_camera_is.append(camera_number)
+            st.write("camera_number", camera_number, "Find!")
+
+        else:
+            st.write("camera_number", camera_number, "None")
+    print("接続されているカメラは", len(true_camera_is), "台です。")
+
+
+if __name__ == "__main__":
+    check_camera_connection()
+    
+    
+    
+    
 kyara = st.sidebar.radio("キャラクター",('あずさ', 'はるか','みお','かなこ','みく','なな'))
 
 
